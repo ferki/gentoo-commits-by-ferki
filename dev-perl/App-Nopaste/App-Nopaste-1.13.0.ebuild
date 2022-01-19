gentoo/gentoo

dev-perl/App-Nopaste: bump version to 1.13.0
Closes: https://bugs.gentoo.org/717082
Package-Manager: Portage-2.3.98, Repoman-2.3.22
Signed-off-by: Ferenc Erki <erkiferenc@gmail.com>

dev-perl/App-Nopaste/App-Nopaste-1.13.0.ebuild
@@ -1,0 +1,54 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=6
+
+DIST_AUTHOR=ETHER
+DIST_VERSION=1.013
+inherit perl-module
+
+DESCRIPTION="Easy access to any pastebin"
+
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~ppc ~x86"
+IUSE="+pastebin +browser clipboard gitlab test"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	dev-perl/Class-Load
+	virtual/perl-Exporter
+	virtual/perl-File-Spec
+	virtual/perl-File-Temp
+	dev-perl/Getopt-Long-Descriptive
+	dev-perl/JSON-MaybeXS
+	dev-perl/libwww-perl
+	dev-perl/Module-Pluggable
+	dev-perl/Module-Runtime
+	dev-perl/Path-Tiny
+	dev-perl/URI
+	dev-perl/WWW-Mechanize
+	>=dev-perl/namespace-clean-0.190.0
+	pastebin? (
+		>=dev-perl/WWW-Pastebin-PastebinCom-Create-1.3.0
+	)
+	clipboard? (
+		dev-perl/Clipboard
+	)
+	gitlab? (
+		dev-vcs/git
+	)
+	browser? (
+		dev-perl/Browser-Open
+	)
+"
+DEPEND="${RDEPEND}
+	>=virtual/perl-CPAN-Meta-Requirements-2.120.620
+	virtual/perl-ExtUtils-MakeMaker
+	virtual/perl-Module-Metadata
+	test? (
+		>=virtual/perl-CPAN-Meta-2.120.900
+		dev-perl/Test-Deep
+		dev-perl/Test-Fatal
+		>=virtual/perl-Test-Simple-0.880.0
+	)
+"

ferki/gentoo-commits-by-ferki

Closes: https://github.com/gentoo/gentoo/pull/15305
Signed-off-by: Kent Fredric <kentnl@gentoo.org>
(cherry picked from commit a74e5b209aaf9942471b96a5c22e8141fd4946c6)

dev-perl/App-Nopaste/App-Nopaste-1.13.0.ebuild
@@ -1,0 +1,54 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DIST_AUTHOR=ETHER
+DIST_VERSION=1.013
+inherit perl-module
+
+DESCRIPTION="Easy access to any pastebin"
+
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~ppc ~x86"
+IUSE="+pastebin +browser clipboard gitlab test"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	dev-perl/Class-Load
+	virtual/perl-Exporter
+	virtual/perl-File-Spec
+	virtual/perl-File-Temp
+	dev-perl/Getopt-Long-Descriptive
+	dev-perl/JSON-MaybeXS
+	dev-perl/libwww-perl
+	dev-perl/Module-Pluggable
+	dev-perl/Module-Runtime
+	dev-perl/Path-Tiny
+	dev-perl/URI
+	dev-perl/WWW-Mechanize
+	>=dev-perl/namespace-clean-0.190.0
+	pastebin? (
+		>=dev-perl/WWW-Pastebin-PastebinCom-Create-1.3.0
+	)
+	clipboard? (
+		dev-perl/Clipboard
+	)
+	gitlab? (
+		dev-vcs/git
+	)
+	browser? (
+		dev-perl/Browser-Open
+	)
+"
+BDEPEND="${RDEPEND}
+	>=virtual/perl-CPAN-Meta-Requirements-2.120.620
+	virtual/perl-ExtUtils-MakeMaker
+	virtual/perl-Module-Metadata
+	test? (
+		>=virtual/perl-CPAN-Meta-2.120.900
+		dev-perl/Test-Deep
+		dev-perl/Test-Fatal
+		>=virtual/perl-Test-Simple-0.880.0
+	)
+"